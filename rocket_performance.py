7747b613c5386f14fbd7c146c722c90358539f63
JohanCarlsen <90179340+JohanCarlsen@users.noreply.github.com>
2021-09-10 10:08:26 +0200

    jobbet videre med oppgave D

--- rocket_performance.py
+++ rocket_performance.py
@@ -4,10 +4,9 @@ rocket_thrust_force = 6*1.4667467764157737e6 # N
 fuel_consumption = 6*1.5382671116999938e-1 # kg/s
 initial_rocket_mass = 1100.0 # kg
 
-# time = 314 # s
-# dt = 0.0001 # s
-# N = int(np.ceil(time/dt))
-
 def fuel_burn(rocket_thrust_force, fuel_consumption, initial_rocket_mass, speed_boost):
-        dv = speed_boost
-        
+        fuel_consumed = 0.
+        F = rocket_thrust_force
+        for i in range(1000):
+            m = initial_rocket_mass-fuel_consumption    # mass whith mass loss
+            a = F/m # acceleration
